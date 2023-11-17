6804660c08b0cd29b9703d616c4324cd56d43d40
etanielo <115602580+etanielo@users.noreply.github.com>
2023-11-16 20:07:02 -0800

    learning rasengan

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems;
 
 public class PivotSubsystem {
-
+    private static PivotSubsystem instance = null;
+    public static PivotSubsystem getInstance() {
+        if(instance == null){
+            instance = new PivotSubsystem();
+        }
+        return instance;
+    }
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems;
 
 public class ShooterSubsystem {
-
+    private static ShooterSubsystem instance = null;
+    public static ShooterSubsystem getInstance() {
+        if(instance == null){
+            instance = new ShooterSubsystem();
+        }
+        return instance;
+    }
 }
