86fa3a070deec2ee4cc8cb46e7cfc5447a1b328b
etanielo <115602580+etanielo@users.noreply.github.com>
2023-11-16 20:00:34 -0800

    start of the training arc

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -3,5 +3,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DrivetrainSubsystem extends SubsystemBase {
-
+    private static DrivetrainSubsystem instance = null;
+    public static DrivetrainSubsystem getInstance() {
+        if(instance == null){
+            instance = new DrivetrainSubsystem();
+        }
+        return instance;
+    }
 }
