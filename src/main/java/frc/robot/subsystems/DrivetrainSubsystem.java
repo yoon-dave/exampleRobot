faa52e704939144831b76bc5df30ef3548f73c8e
dave : ) <davegunwoo@gmail.com>
2023-11-17 19:49:39 -0800

    dfdsa

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,43 +1,20 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.kauailabs.navx.frc.AHRS;
-import com.swervedrivespecialties.swervelib.Mk4ModuleConfiguration;
-import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
-import com.swervedrivespecialties.swervelib.SwerveModule;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants;
-import frc.robot.RobotContainer;
-
 
 public class DrivetrainSubsystem extends SubsystemBase {
     private static DrivetrainSubsystem instance = null;
 
     public static DrivetrainSubsystem getInstance() {
 
-        if(instance == null){
+        if (instance == null) {
             instance = new DrivetrainSubsystem();
 
         }
 
         return instance;
     }
+
     private static SwerveModule m_frontLeftModule;
     private static SwerveModule m_frontRightModule;
     private static SwerveModule m_backLeftModule;
