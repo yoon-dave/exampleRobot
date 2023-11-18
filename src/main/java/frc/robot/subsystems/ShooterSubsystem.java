804a82cf63e16f7e711bc0893389845190f7249d
dave : ) <davegunwoo@gmail.com>
2023-11-18 09:12:28 -0800

    commit

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,19 +5,20 @@ import frc.robot.drivers.LazyTalonFX;
 
 public class ShooterSubsystem {
     private static ShooterSubsystem instance = null;
+
     public static ShooterSubsystem getInstance() {
-        if(instance == null){
+        if (instance == null) {
             instance = new ShooterSubsystem();
         }
         return instance;
     }
 
-    private final LazyTalonFX mLeaderShooter
+    private final LazyTalonFX mLeaderShooter;
     private final LazyTalonFX mFollowerShooter;
 
     public ShooterSubsystem() {
         mLeaderShooter = TalonFXFactory.createDefaultFalcon("Leader Shooter", 0);
         mFollowerShooter = TalonFXFactory.createSlaveFalcon("Folllower Shooter", 1, 0);
     }
-    
+
 }
