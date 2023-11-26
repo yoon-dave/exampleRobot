93c5d8ed076b5ed2eab6734045cb43bb0d68f042
etanielo <115602580+etanielo@users.noreply.github.com>
2023-11-25 23:47:07 -0800

    a

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.subsystems.SwerveModule;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 
 public class DrivetrainSubsystem extends SubsystemBase {
     private static DrivetrainSubsystem instance = null;
@@ -13,8 +15,13 @@ public class DrivetrainSubsystem extends SubsystemBase {
         }
         return instance;
     }
-    // private static SwerveModule m_frontLeftModule;
-    // private static SwerveModule m_frontRightModule;
-    // private static SwerveModule m_backLeftModule;
-    // private static SwerveModule m_backRightModule;
+    private static SwerveModule m_frontLeftModule;
+    private static SwerveModule m_frontRightModule;
+    private static SwerveModule m_backLeftModule;
+    private static SwerveModule m_backRightModule;
+
+    private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+    private Field2d m_field = new Field2d();
+    private Field2d m_hub = new Field2d();
+
 }
