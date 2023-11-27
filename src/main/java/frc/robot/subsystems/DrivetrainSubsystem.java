2bed689c6e4aeb50643f3fa2f596c3d5e40508a2
etanielo <115602580+etanielo@users.noreply.github.com>
2023-11-27 00:39:41 -0800

    we must fix swervemodule imports for code to work

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -6,6 +6,8 @@ import frc.robot.subsystems.SwerveModule;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import com.swervedrivespecialties.swervelib.Mk4ModuleConfiguration;
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
 
 public class DrivetrainSubsystem extends SubsystemBase {
     private static DrivetrainSubsystem instance = null;
@@ -30,4 +32,28 @@ public class DrivetrainSubsystem extends SubsystemBase {
     private Field2d m_field = new Field2d();
     private Field2d m_hub = new Field2d();
 
+    public interface SwerveModule {
+        double getDriveVelocity();
+    
+        double getSteerAngle();
+    
+        void set(double driveVoltage, double steerAngle);
+    }
+
+    //add default command (teleop)
+    //basically all parameters need to be added (fix all null and 0 values)
+    //need to add parameters for swervemodules
+    public DrivetrainSubsystem() {
+        setDefaultCommand(null);
+
+        Mk4ModuleConfiguration driveConfig = new Mk4ModuleConfiguration(null);
+        m_swerveDriveOdometry = new SwerveDriveOdometry(null, null);
+        m_feedforward = new SimpleMotorFeedforward(0, 0, 0);
+
+        m_frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(driveConfig, 0, 0, 0, 0); //parameters should be drive and steer motor ports, encoder port, steer offset
+        m_frontRightModule = Mk4SwerveModuleHelper.createFalcon500(driveConfig, 0, 0, 0, 0);
+        m_backLeftModule = Mk4SwerveModuleHelper.createFalcon500(driveConfig, 0, 0, 0, 0);
+        m_backRightModule = Mk4SwerveModuleHelper.createFalcon500(driveConfig, 0, 0, 0, 0);
+    }
+    
 }
